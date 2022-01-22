25780610c40bcea23ece549613c101d6d4519959
cnohall <christopher.nohall@gmail.com>
2022-01-22 14:49:54 +0900

    Re-organize navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -6,13 +6,15 @@ export default function Navigationbar() {
   return (
     <Navbar variant='dark'>
         <Container >
-            <Link href="/"passHref><Navbar.Brand>{'Chris Nohall'}</Navbar.Brand></Link>
+            {/* <Link href="/"passHref><Navbar.Brand>{'Chris Nohall'}</Navbar.Brand></Link> */}
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">       
-                <Link href="/work"passHref><Nav.Link>Work</Nav.Link></Link>
-                <Link href="/resume"passHref><Nav.Link>Resume</Nav.Link></Link>
-                <Link href="/contact"passHref><Nav.Link>Contact</Nav.Link></Link>
+            <Nav className="m-auto">       
+              <Link href="/"passHref><Nav.Link>Home</Nav.Link></Link>
+              <Link href="/work"passHref><Nav.Link>Work</Nav.Link></Link>
+              <Link href="/writing"passHref><Nav.Link>Writing</Nav.Link></Link>
+              <Link href="/projects"passHref><Nav.Link>Projects</Nav.Link></Link>
+              <Link href="/contact"passHref><Nav.Link>Contact</Nav.Link></Link>
             </Nav>
             </Navbar.Collapse>
         </Container>
--- pages/writing.tsx
+++ pages/writing.tsx
@@ -2,16 +2,16 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-export default function Work() {
+export default function Writing() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>{`Chris Nohall's Work Experience`}</title>
+        <title>{`Chris Nohall's Writing`}</title>
         {/* Change this for create better SEO */}
-        <meta name="description" content="Chris Nohall's Work Experience" />
+        <meta name="description" content="Posts written by Chris Nohall" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1 className='my-3'>Work</h1>
+      <h1 className='my-3'>Writing</h1>
     </div>
   )
 }
