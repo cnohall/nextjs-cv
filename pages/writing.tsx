aee6b790a8d635a61595b024a68a4ca8a299d0cc
cnohall <christopher.nohall@gmail.com>
2022-01-23 16:29:02 +0900

    add writing section

--- pages/writing.tsx
+++ pages/writing.tsx
@@ -1,17 +1,44 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import { getSortedPostsData } from '../lib/posts'
+import { Container, Card } from 'react-bootstrap'
+import Link from 'next/link'
 
-export default function Writing() {
+export default function Writing({ allPostsData }) {
   return (
-    <div className={styles.container}>
+    <Container>
       <Head>
         <title>{`Chris Nohall's Writing`}</title>
-        {/* Change this for create better SEO */}
-        <meta name="description" content="Posts written by Chris Nohall" />
-        <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1 className='my-3'>Writing</h1>
-    </div>
+      <h3 className='my-5'>{`Welcome to my writting corner! I write about coding, with an easy-going and personal perspective.`}</h3>
+        {allPostsData.map(({ id, date, title }) => (
+          <Link href={`/writing/${id}`} passHref key={id}>
+            <Card 
+              style={{ width: '100%', 'border-bottom': 'solid'}} 
+              className='my-3'
+              role='button'
+              text='white'
+              bg='secondary' 
+            >
+              <Card.Body>
+                <Card.Title>{title}</Card.Title>
+                <Card.Text>
+                  {`Some quick example text to build on the card title and make up the bulk of
+                  the card's content.`}
+                </Card.Text>
+                <Card.Text>{date}</Card.Text>
+              </Card.Body>
+            </Card>
+          </Link>
+        ))}
+    </Container>
   )
+}
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
 }
