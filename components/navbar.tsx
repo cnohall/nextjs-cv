14740b653feace962e66b9e22f4a831a124caa4e
cnohall <christopher.nohall@gmail.com>
2022-02-26 10:51:31 +0900

    add navbar translations

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,14 +1,16 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap';
-import { LANGUAGES } from '../helpers/constants';
+import { I18N_NAMESPACES, LANGUAGES } from '../helpers/constants';
 
 import { useEffect, useState } from 'react';
 import i18next from 'i18next';
+import { useTranslation } from 'react-i18next';
 
 export default function Navigationbar() {
 
   const [language, setLanguage] = useState();
+  const { t } = useTranslation(I18N_NAMESPACES.COMMON);
 
   useEffect(() => {
     i18next.changeLanguage(language);
@@ -26,23 +28,23 @@ export default function Navigationbar() {
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="m-auto">       
-              <Link href="/" passHref><Nav.Link>Home</Nav.Link></Link>
-              <Link href="/work" passHref><Nav.Link>Work</Nav.Link></Link>
-              <Link href="/writing" passHref><Nav.Link>Writing</Nav.Link></Link>
-              <Link href="/projects" passHref><Nav.Link>Projects</Nav.Link></Link>
-              <Link href="/contact" passHref><Nav.Link>Contact</Nav.Link></Link>
-              <NavDropdown title="Languages" id="basic-nav-dropdown">
+              <Link href="/" passHref><Nav.Link>{t('common:home')}</Nav.Link></Link>
+              <Link href="/work" passHref><Nav.Link>{t('common:work')}</Nav.Link></Link>
+              <Link href="/writing" passHref><Nav.Link>{t('common:writing')}</Nav.Link></Link>
+              <Link href="/projects" passHref><Nav.Link>{t('common:projects')}</Nav.Link></Link>
+              <Link href="/contact" passHref><Nav.Link>{t('common:contact')}</Nav.Link></Link>
+              <NavDropdown title={t('common:languages')} id="basic-nav-dropdown">
                 <NavDropdown.Item onClick={() => changeLanguage(LANGUAGES.zh)}>
-                  Chinese (Mandarin)
+                  {t('common:chinese')}
                 </NavDropdown.Item>
                 <NavDropdown.Item onClick={() => changeLanguage(LANGUAGES.en)}>
-                  English
+                  {t('common:english')}
                 </NavDropdown.Item>
                 <NavDropdown.Item onClick={() => changeLanguage(LANGUAGES.kr)}>
-                  Korean
+                  {t('common:korean')}
                 </NavDropdown.Item>
                 <NavDropdown.Item onClick={() => changeLanguage(LANGUAGES.sv)}>
-                  Swedish
+                  {t('common:swedish')}
                 </NavDropdown.Item>
               </NavDropdown>
             </Nav>
