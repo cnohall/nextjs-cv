15ef412507f1f27549b8fd251117d8f55e96bc2b
cnohall <christopher.nohall@gmail.com>
2024-07-29 13:47:46 +0900

    temp

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -6,8 +6,9 @@ import styles from '../styles/Home.module.css';
 import emailjs from '@emailjs/browser';
 import BootstrapToast from '../components/toast';
 
-const messagePlaceholder = 'Hi, we need a frontend developer to on our website at Company X. ' 
-  + 'How soon can we hop on a call to discuss this?';
+const messagePlaceholder =
+  'Hi, we need a frontend developer to on our website at Company X. ' +
+  'How soon can we hop on a call to discuss this?';
 
 export default function Contact() {
   const form = useRef();
@@ -17,34 +18,38 @@ export default function Contact() {
 
   const sendEmail = (e) => {
     e.preventDefault();
-    if(sentEmail) {
+    if (sentEmail) {
       setMessage({
-        title: 'Again?', 
-        comment: 'woops!', 
-        body: 'You have already sent an email'
+        title: 'Again?',
+        comment: 'woops!',
+        body: 'You have already sent an email',
       });
       setShowToast(true);
       return;
-    } 
+    }
     const userId = process.env.NEXT_PUBLIC_USER_ID;
     const serviceId = process.env.NEXT_PUBLIC_SERVICE_ID;
     const templateId = process.env.NEXT_PUBLIC_TEMPLATE_ID;
-    emailjs.sendForm(serviceId, templateId, form.current, userId)
-      .then((result) => {
-          setMessage({
-            title: 'Your message is on the way', 
-            comment: 'Success', 
-            body: 'Thanks for getting in touch'
-          });
-          setShowToast(true);
-          setSentEmail(true);
-      }, (error) => {
+    emailjs.sendForm(serviceId, templateId, form.current, userId).then(
+      (result) => {
+        setMessage({
+          title: 'Your message is on the way',
+          comment: 'Success',
+          body: 'Thanks for getting in touch',
+        });
+        setShowToast(true);
+        setSentEmail(true);
+      },
+      (error) => {
         setMessage({
           title: 'Sorry, something went wrong',
           comment: 'Error',
-          body: 'I\'m sorry for the inconvinience.'});
+          // eslint-disable-next-line quotes
+          body: "I'm sorry for the inconvinience.",
+        });
         setShowToast(true);
-      });
+      },
+    );
   };
   return (
     <Container className={styles.center}>
@@ -56,51 +61,58 @@ export default function Contact() {
       </Head>
       <Row xs={1} sm={2}>
         <Col>
-          <h1 className='my-'>Contact Me</h1>
-          <h4 className='my-4'>
+          <h1 className="my-">Contact Me</h1>
+          <h4 className="my-4">
             {'Want to say "Hey!"? Got something you\'d like to ask? '}
             {'New project or opportunity? Want to hire me? Contact me!'}
           </h4>
-          <h4 className='my-4'>
+          <h4 className="my-4">
             Use the form. Alternatively, shoot me an email at christopher.nohall@gmail.com.
           </h4>
-          <h4 className='my-4'>Prefer a face to face? Schedule a call.</h4>
+          <h4 className="my-4">Prefer a face to face? Schedule a call.</h4>
         </Col>
         <Col>
-        { showToast &&
-          <BootstrapToast message={message} setShowToast={setShowToast}/>
-        }
-        <Form ref={form} onSubmit={sendEmail}>
-          <Row xs={1} sm={2}>
-            <Col>
-              <Form.Group className="my-2" >
-                <Form.Label>Your Name</Form.Label>
-                <Form.Control required name="from_name" type="text" placeholder="Enter your name" />
-              </Form.Group>
-            </Col>
-            <Col>
-              <Form.Group className="my-2">
-                <Form.Label>Email Address</Form.Label>
-                <Form.Control required name="reply_to" type="email" placeholder="Enter your email address" />
-              </Form.Group>
-            </Col>
-          </Row>
-          <Form.Group className="mb-3">
-            <Form.Label>Your Message</Form.Label>
-            <Form.Control 
-              required 
-              name="message" 
-              as="textarea" 
-              rows={3}
-              placeholder={messagePlaceholder} 
-            />
-          </Form.Group>
-          <Button variant="secondary" type="submit" >
-            Submit
-          </Button>
-        </Form>
+          {showToast && <BootstrapToast message={message} setShowToast={setShowToast} />}
+          <Form ref={form} onSubmit={sendEmail}>
+            <Row xs={1} sm={2}>
+              <Col>
+                <Form.Group className="my-2">
+                  <Form.Label>Your Name</Form.Label>
+                  <Form.Control
+                    required
+                    name="from_name"
+                    type="text"
+                    placeholder="Enter your name"
+                  />
+                </Form.Group>
+              </Col>
+              <Col>
+                <Form.Group className="my-2">
+                  <Form.Label>Email Address</Form.Label>
+                  <Form.Control
+                    required
+                    name="reply_to"
+                    type="email"
+                    placeholder="Enter your email address"
+                  />
+                </Form.Group>
+              </Col>
+            </Row>
+            <Form.Group className="mb-3">
+              <Form.Label>Your Message</Form.Label>
+              <Form.Control
+                required
+                name="message"
+                as="textarea"
+                rows={3}
+                placeholder={messagePlaceholder}
+              />
+            </Form.Group>
+            <Button variant="secondary" type="submit">
+              Submit
+            </Button>
+          </Form>
         </Col>
-
       </Row>
     </Container>
   );
