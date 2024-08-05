4bbed5beff5deabefdbcd01edfa38e7d2c16fec2
cnohall <christopher.nohall@gmail.com>
2024-08-05 23:45:32 +0900

    temp

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,11 +1,11 @@
 module.exports = {
-    i18n: {
-      defaultLocale: 'en',
-      locales: ['en', 'kr', 'sv', 'zh'],
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en', 'kr', 'sv', 'zh'],
   },
   localePath:
     typeof window === 'undefined'
-      ? require('path').resolve('./my-custom/path')
-      : '/public/my-custom/path',
+      ? require('path').resolve('./public/locales')
+      : '/public/locales/',
   ns: ['common'],
 };
--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 const { i18n } = require('./next-i18next.config');
 
 module.exports = {
+  compiler: {
+    styledComponents: true,
+  },
   i18n,
 };
