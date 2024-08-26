071a95a339462666d35511f26f57634dd381b516
cnohall <christopher.nohall@gmail.com>
2024-08-26 11:45:24 +0900

    init

---
+++ components/atoms/Loader/index.tsx
@@ -0,0 +1,21 @@
+import { Html, useProgress } from '@react-three/drei';
+
+const Loader = () => {
+  const { progress } = useProgress();
+  return (
+    <span className="canvas-load">
+      <p
+        style={{
+          fontSize: 14,
+          color: '#f1f1f1',
+          fontWeight: 800,
+          marginTop: 40,
+        }}
+      >
+        {progress.toFixed(2)}
+      </p>
+    </span>
+  );
+};
+
+export default Loader;
